7272f65c31acfb23ec881b2690f082a0b339fc4f
Chugo <chris.couchalot@gmail.com>
2025-09-15 13:30:29 +0200

    hello

--- tema01/tema01/oppgave2.php
+++ tema01/tema01/oppgave2.php
@@ -3,5 +3,5 @@
 $fornavn=$_POST ["fornavn"];
 $etternavn=$_POST ["etternavn"];
 $alder=$_POST ["alder"]; /* variable gitt verdier fra feltene i HTML-skjemaet */
-print ("God dag $fornavn $etternavn, du er $alder &aring;r og like sprek. <br />"); /* resultat skrevet ut */
+print ("God dag $fornavn $etternavn, du er $alder. <br />"); /* resultat skrevet ut */
 ?>
