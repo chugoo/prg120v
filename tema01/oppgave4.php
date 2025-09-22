b6ba7a185f368d5d49db074f95e030fc1cecc080
Chugo <chris.couchalot@gmail.com>
2025-09-22 11:25:51 +0200

    hei

--- tema01/oppgave4.php
+++ tema01/oppgave4.php
@@ -1,8 +1,5 @@
 <?php /* Oppgave 4 */
-/*
-/* Programmet mottar 2 tall fra et HTML-skjema ved POST-metoden
-/* Programmet skriver ut de 2 tallene og summen, differansen, produktet og kvotientet av/mellom tallene
-*/
+
 $tall1=$_POST ["tall1"];
 $tall2=$_POST ["tall2"]; /* variable gitt verdier fra feltene i HTML-skjemaet */
 $sum=$tall1 + $tall2;
