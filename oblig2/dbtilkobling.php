0198d1532ac22349e8873701cf0301c49f9ff1ae
Chugo <chris.couchalot@gmail.com>
2025-09-22 13:52:21 +0200

    feil

--- oblig2/dbtilkobling.php
+++ oblig2/dbtilkobling.php
@@ -1,7 +1,11 @@
-<?php 
- $host="localhost";
- $user="140478";
- $password="c993140478";
- $database="140478"; 
- $db=mysqli_connect($host,$user,$password,$database) or die ("ikke kontakt med database-server");
+<?php /* dbtilkobling */
+/*
+/* Programmet foretar tilkobling til database-server og valg av database
+*/
+$host = getenv('DB_HOST');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+$database = getenv('DB_DATABASE');
+$db=mysqli_connect($host,$username,$password,$database) or die ("ikke kontakt med database-server");
+/* tilkobling til database-serveren utført */
 ?>
